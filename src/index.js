69e8c5ba8705a0375e8e51557c34bd73b2aaa744
mmoramova <93440480+mmoramova@users.noreply.github.com>
2022-02-12 13:28:15 +0100

    Updated time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+function formatDate(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes} `;
+}
+
 function displayTemperature(response) {
   console.log(response);
   console.log(response.data.main.temp);
@@ -6,11 +31,13 @@ function displayTemperature(response) {
   console.log(response.data.main.humidity);
   console.log(response.data.wind.speed);
   let cityElement = document.querySelector("#city");
+  let dateElement = document.querySelector("#date");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let temperatureElement = document.querySelector("#temperature");
   let windElement = document.querySelector("#wind");
   cityElement.innerHTML = response.data.name;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -22,3 +49,5 @@ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q= New York&units=
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
+
+new Date(100 * 1000);
